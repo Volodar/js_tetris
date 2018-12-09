3b976fe72f082c70e6e20d1f8a6c9b9074e8be63
VladimirTolmachev(Home OSX) <tolm_vl@hotmail.com>
2018-12-09 22:59:01 +0700

    move a sources to src folder

--- scr/viewes.js
+++ scr/viewes.js
@@ -1,38 +1,4 @@
-const GRID_SIZE = 32;
-const CELL_EMPTY = 0;
-const COLORS = {
-    PURPLE: "#d535b2",
-    LIGHT_BLUE: "#00acfe",
-    BLUE: "#0f47cd",
-    RED: "#e60030",
-    PINK: "#ef2c53",
-    YELLOW: "#ffae1c",
-    ORANGE: "#ff7b18",
-    GREEN: "#64d514",
-    LIGHT_GRAY: "#a0a0a0",
-    DARK_GRAY: "#404040",
-    GRAY: "#808080",
-    WHITE: "#ffffff",
-};
 
-class GameBoard{
-    constructor(){
-        this.width = 10;
-        this.height = 20;
-        this.cells = new Array(this.width);
-        for(let i=0; i<this.width; ++i){
-            this.cells[i] = new Array(this.height);
-            for(let j=0; j<this.height; ++j){
-                this.cells[i][j] = CELL_EMPTY;
-            }
-        }
-
-        for(let i=0; i<this.width; ++i) {
-            this.cells[i][0] = COLORS.RED;
-            this.cells[i][1] = COLORS.BLUE;
-        }
-    }
-}
 
 class GameScene extends Scene {
     constructor(){
@@ -113,28 +79,5 @@ class BlockView extends Node {
         let [X, Y] = GameScene.get_zero_position();
         this.x = X + (i + 0.5) * GRID_SIZE;
         this.y = Y - (j + 0.5) * GRID_SIZE;
-    }
-}
-
-
-class Figure extends Node {
-
-    constructor(forms) {
-        super();
-        this.x = 0;
-        this.y = 0;
-        this.form = forms[3];
-        this.figure_coords = [0, 0];
-        this.coords = [];
-        this.form.forEach( (l, i) => {
-            const row = this.form[i];
-
-            row.forEach((i, j) => {
-                if (i > 0) {
-                    const coord = [i, j];
-                    this.coords.push(coord);
-                }
-            });
-        });
     }
 }
