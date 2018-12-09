4707b470cdb08ddee1be356f5aee439b7a8d57bf
tany <valehova.tany@gmail.com>
2018-12-09 12:26:06 +0700

    добавила массив с описанием фигур

--- main.js
+++ main.js
@@ -4,3 +4,102 @@ game.root.add_child(new Sprite(200, 100, 'assets/block0.png'));
 game.root.add_child(new Sprite(264, 100, 'assets/block0.png'));
 game.root.add_child(new Sprite(328, 100, 'assets/block0.png'));
 game.root.add_child(new Sprite(328, 164, 'assets/block0.png'));
+
+
+const figures = [
+    [
+        [1, 1],
+        [0, 1, 1],
+        {
+            rotatable: true,
+        }
+    ],
+    [
+        [1],
+        [1],
+        [1],
+        [1],
+        {
+            rotatable: true,
+        }
+    ],
+    [
+        [1, 0, 1],
+        [1, 1, 1],
+        {
+            rotatable: true,
+        }
+    ],
+    [
+        [0, 1, 0],
+        [1, 1, 1],
+        {
+            rotatable: true,
+        }
+    ],
+    [
+        [0, 1, 1],
+        [1, 1],
+        {
+            rotatable: true,
+        }
+    ],
+    [
+        [1, 1],
+        [1],
+        [1],
+        {
+            rotatable: true,
+        }
+    ],
+    [
+        [1, 1],
+        [1, 1],
+        {
+            rotatable: false
+        }
+    ],
+    [
+        [1, 1, 1],
+        [0, 0, 1],
+        {
+            rotatable: true,
+        }
+    ],
+    [
+        [1],
+        [1, 1],
+        {
+            rotatable: true,
+        }
+    ],
+    [
+        [0, 1],
+        [1, 1],
+        {
+            rotatable: true,
+        }
+    ],
+    [
+        [1],
+        {
+            rotatable: false
+        }
+    ],
+    [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 1, 0],
+        {
+            rotatable: false
+        }
+    ],
+    [
+        [1, 1],
+        {
+            rotatable: true,
+        }
+    ]
+];
+
+
